3f57fc4cf772a723e1d190384216cdcfbc4e0369
nvtrireme <nischay.vaish@triremetrading.com>
2025-04-12 10:11:30 +0530

    message sender contract

---
+++ foundry/src/MessageStore.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract MessageStore is Ownable {
+    string private message;
+
+    event MessageStored(address indexed sender, string message);
+
+    constructor() Ownable(msg.sender) {}
+
+    function storeMessage(string calldata newMessage) external onlyOwner {
+        require(bytes(newMessage).length > 0, "Message cannot be empty");
+        message = newMessage;
+        emit MessageStored(msg.sender, newMessage);
+    }
+
+    function retrieveMessage() external view returns (string memory) {
+        return message;
+    }
+}
